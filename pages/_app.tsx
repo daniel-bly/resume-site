cf9eb094912de4063c44a331f933111a10213bfa
Daniel Bly <danielbly999@gmail.com>
2023-01-25 13:01:02 +1300

    minor fix: updated icon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,17 @@ import '../styles/globals.css'
 import "../styles/landing.css"
 import 'normalize.css/normalize.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Daniel Bly | Full Stack Developer</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
