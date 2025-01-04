dc137f7e2e7359d9a0e2e037fda8b680888440d1
Dan <dan.bly@futureco.co>
2025-01-04 16:26:26 +1300

    Updated site to display 'Coming Soon'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect, useRef } from 'react'
+import { useCallback, useState, useEffect, useRef, LegacyRef, RefObject, MutableRefObject } from 'react'
 import type { NextPage } from 'next'
 import Image from 'next/image'
 import Particles from 'react-tsparticles'
@@ -174,13 +174,15 @@ const Home: NextPage = () => {
     setExpanded(newExpanded ? panel : false);
   }
 
-  const aboutRef = useRef(null)
-  const projectsRef = useRef(null)
-  const contactRef = useRef(null)
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLElement>();
+  const contactRef = useRef<HTMLElement>();
 
-  const [active, setActive] = useState('home')
+  const [active, setActive] = useState('home');
 
   const handleScroll = () => {
+    if(!aboutRef.current || !projectsRef.current || !contactRef.current) return setActive('home');
+
     const headerHeight = 60
     const { top, height } = document?.body.getBoundingClientRect() ?? 0
     setActive('contacts')
@@ -205,6 +207,8 @@ const Home: NextPage = () => {
     console.log(active)
   }, [active])
 
+  const [isAnimComplete, setAnimComplete] = useState(false);
+
   return (
     <div style={{backgroundColor: 'black'}}>
     <div className="anchor" id="home" />
@@ -218,132 +222,16 @@ const Home: NextPage = () => {
               .typeString('<span>Hi,<br/> I am <span style="color: #ffd400;">Daniel</span></span>')
               .pauseFor(500)
               .typeString("<br/><span>A Full-Stack SaaS Developer</span>")
+              .callFunction(() => setAnimComplete(true))
               .start()
           }} />
+          {isAnimComplete && <div style={{ fontSize: "1.6rem", fontWeight: 300, marginTop: 10, animation: "3s ease 0s normal forwards 1 fadein" }}>Coming Soon...</div>}
           </div>
       </div>  
-      <a href="#about">
+      <a href="#contact">
         Learn More
         <Image src="/res/down.svg" alt="Cross" width={15} height={15} />
       </a>
-    </div>
-    {/* <div className={styles.background_particles} >
-      <Particles id="tsparticles2" init={particlesInit} loaded={particlesLoaded} options={options} canvasClassName="particlesCanvas" />
-    </div> */}
-    <Header active={active} />
-    <div className="anchor" id="about" />
-    <div className="about-wrapper" ref={aboutRef}>
-      <h1 className="heading">About</h1>
-      <div className="flex-wrapper">
-        <div className="text-wrapper">
-          <Image className="image" src="/Profile.svg" width={300} height={300} />
-          <p>Since learning programming during my childhood as a way to create my own video games, coding has become part of my daily life.</p>
-          <p>As a freelance Full-Stack SaaS developer I have further harnessed my love of learning to develop skills in a wide range of technologies and frameworks, allowing me to deliver the best solution to my clients.</p>
-          <p>When I am not at my computer I enjoy reading, SIM racing, tabletop gaming and playing guitar or piano.</p>
-        </div>
-        <div className="skills-wrapper">
-          <CustomAccordion 
-            title="Languages" 
-            expanded={exapanded == 'panel1'} 
-            handleChange={() => handleChange('panel1')}
-            id="panel1">
-            <Skill logo="HTML5.svg" alt="HTML5 logo" text="HTML5" />
-            <Skill logo="CSS3.svg" alt="CSS3 logo" text="CSS3" />
-            <Skill logo="PHP.svg" alt="PHP logo" text="PHP" />
-            <Skill logo="Javascript.svg" alt="Javascript logo" text="Javascript" />
-            <Skill logo="Typescript.svg" alt="Typescript logo" text="Typescript" />
-            <Skill logo="CSharp.svg" alt="C# logo" text="C#" />
-            <Skill logo="Java.svg" alt="Java logo" text="Java" />
-          </CustomAccordion>
-          <CustomAccordion 
-            title="Frameworks" 
-            expanded={exapanded === 'panel2'} 
-            handleChange={() => handleChange('panel2')}
-            id="panel2">
-            <Skill logo="React.svg" alt="React logo" text="ReactJS" />
-            <Skill logo="NextJS.svg" alt="NextJS logo" text="NextJS" invert />
-            <Skill logo="Laravel.svg" alt="Laravel logo" text="Laravel" />
-            <Skill logo="NodeJS.svg" alt="NodeJS logo" text="NodeJS" />
-            <Skill logo="ExpressJS.svg" alt="ExpressJS logo" text="ExpressJS" invert />
-            <Skill logo="NestJS.svg" alt="NestJS logo" text="NestJS" />
-            <Skill logo="RubyOnRails.svg" alt="Ruby on Rails logo" text="Ruby on Rails" />
-          </CustomAccordion>
-          <CustomAccordion 
-            title="Databases" 
-            expanded={exapanded === 'panel3'} 
-            handleChange={() => handleChange('panel3')}
-            id="panel3">
-            <Skill logo="MongoDB.svg" alt="MongoDB logo" text="MongoDB" />
-            <Skill logo="PostgreSQL.svg" alt="PostgreSQL logo" text="PostgreSQL" />
-            <Skill logo="Neo4j.svg" alt="Neo4j logo" text="Neo4j" />
-            <Skill logo="MySQL.svg" alt="MySQL logo" text="MySQL" />
-          </CustomAccordion>
-          <CustomAccordion 
-            title="DevOps" 
-            expanded={exapanded === 'panel4'} 
-            handleChange={() => handleChange('panel4')}
-            id="panel4">
-            <Skill logo="Git.svg" alt="Git logo" text="Git" />
-            <Skill logo="Docker.svg" alt="Docker logo" text="Docker" />
-            <Skill logo="ci-cd.svg" alt="CI/CD logo" text="CI/CD" invert />
-            <Skill logo="jest.svg" alt="Jest logo" text="JestJS" />
-          </CustomAccordion>
-        </div>
-      </div>
-    </div>
-    <div className="anchor" id="projects" />
-    <div className="projects-wrapper" ref={projectsRef}>
-      <h1 className="heading">Projects</h1>
-      <InfoPopup title="Not Avaliable" setInfoPopup={setInfoPopup1} showPopup={infoPopup1}>
-        This application includes proprietary software I do not have permission to host. If you would like me to request special permission from the owner please let me know.
-      </InfoPopup>
-      <div className="project">
-        <a onClick={() => setInfoPopup1(true)} className="image-wrapper">
-          <Image src="/commuter-v2-site-capture.JPG" alt="Commuter v2 Screenshot" width={740} height={360} layout="responsive" />
-        </a>
-        <div className="text-wrapper">
-          <h2>Commuter v2</h2>
-          <p>Full stack application created with PHP7 and Laravel designed to refresh Skycity’s outdated fleet management system.</p>
-          <a onClick={() => setInfoPopup1(true)}><div className="link-button">LIVE APP</div></a>
-          <a onClick={() => addGitHubColaborator('commute-v2')}><div className="link-button">VIEW CODE</div></a>
-        </div>
-      </div>
-      <div className="project inverted">
-        <div className="text-wrapper">
-          <h2>Flicka NZ</h2>
-          <p>An online marketplace with a rewards system which allows the user to take place in draws to win prizes. Created using Ruby on Rails with a PostgreSQL database and a React front-end utilizing Firebase for file storage.</p>
-          <a href="https://danielbly.dev/flicka" target="_blank"><div className="link-button">LIVE APP</div></a>
-          <a onClick={() => addGitHubColaborator('flicka')}><div className="link-button">VIEW CODE</div></a>
-        </div>
-        <a href="https://danielbly.dev/flicka" target="_blank" className="image-wrapper">
-          <Image src="/flicka-site-capture.JPG" alt="Flicka Screenshot" width={740} height={360} layout="responsive" />
-        </a>
-      </div>
-      <div className="project">
-        <a href="https://beta.dataset.co.nz" target="_blank" className="image-wrapper">
-          <Image src="/dataset-site-capture.JPG" alt="Dataset Screenshot" width={740} height={360} layout="responsive" />
-        </a>
-        <div className="text-wrapper">
-          <h2>Dataset (Beta)</h2>
-          <p>A social media application designed to aid in the discovery of up-and-coming electronic music artists. Created using Next.js and Express.js with a Neo4j graph database and AWS S3 file storage.</p>
-          <a href="https://beta.dataset.co.nz" target="_blank"><div className="link-button">LIVE APP</div></a>
-          <a onClick={() => addGitHubColaborator('dataset')}><div className="link-button">VIEW CODE</div></a>
-        </div>
-      </div>
-      <InfoPopup title="Coming Soon!" setInfoPopup={setInfoPopup2} showPopup={infoPopup2}>
-        This application is currently in development and therefore no live demo is avaliable.
-      </InfoPopup>
-      <div className="project inverted last">
-        <div className="text-wrapper">
-          <h2>Commuter v3 (WIP)</h2>
-          <p>I am currently developing Commuter v3 in partnership with it’s owner as modular cooperate booking software. It is being created in Typescript with a Nest.js backend running a MongoDB database and will implement a Next.js frontend.</p>
-          <a onClick={() => setInfoPopup2(true)}><div className="link-button">LIVE APP</div></a>
-          <a onClick={() => addGitHubColaborator('commute-v3')}><div className="link-button">VIEW CODE</div></a>
-        </div>
-        <a onClick={() => setInfoPopup2(true)} className="image-wrapper">
-          <Image src="/wip.jpg" alt="Flicka Screenshot" width={740} height={360} layout="responsive" />
-        </a>
-      </div>
     </div>
 
     <LoadingPopup showPopup={loadingPopup} />
@@ -354,20 +242,20 @@ const Home: NextPage = () => {
       <h1 className="heading">Contact</h1>
       <div className="flex-wrapper">
         <div className="text-wrapper">
-          <p>Fill out the form bellow and I will get back to you as soon as possible! You can also <a href="/downloads/daniel-bly-developer-cv-2023.pdf" download={'daniel-bly-developer-cv-2023.pdf'}>download a copy of my CV.</a></p>
+          <p>Fill out the form bellow and I will get back to you as soon as possible! {/* You can also <a href="/downloads/daniel-bly-developer-cv-2023.pdf" download={'daniel-bly-developer-cv-2023.pdf'}>download a copy of my CV.</a> */}</p>
         </div>
         <div className="form-wrapper">
           <form className="w-100" onSubmit={handleSubmit(onSubmit)}>
               <div className="form-group w-100">
-                  <input className="form-control w-100" placeholder='Name' name="name" {...register('name')} />
+                  <input className="form-control w-100" placeholder='Name' {...register('name')} />
                   <div className="invalid-feedback">{errors.name?.message?.toString()}</div>
               </div>
               <div className="form-group">
-                  <input className="form-control w-100" placeholder="Email" name="email" {...register('email')} />
+                  <input className="form-control w-100" placeholder="Email" {...register('email')} />
                   <div className="invalid-feedback">{errors.email?.message?.toString()}</div>
               </div>
               <div className="form-group">
-                  <textarea className="form-control w-100" placeholder="Message" name="message"  {...register('message')} />
+                  <textarea className="form-control w-100" placeholder="Message" {...register('message')} />
                   <div className="invalid-feedback">{errors.message?.message?.toString()}</div>
               </div>
               <input className="button" type="SUBMIT" />
